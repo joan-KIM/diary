869b9510db984e9f306feb3bbb5f17fc31294e92
joan-KIM <3373minji.kim@gmail.com>
2021-11-03 16:47:35 +0900

    Date 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './App.css';
 import Button from './components/Button';
+import Date from "./components/Date";
 
 function App() {
   return (
     <div className="App">
+      <Date />
       <Button title={"Add Task"}/>
     </div>
   );
---
+++ src/components/Date.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Date(){
+    return(
+        <div className='date-wrapper'>
+            <p className='date'>09</p>
+            <p className='day'>Sunday</p>
+        </div>
+    )
+}
