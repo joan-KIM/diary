0ffb7c6da4924f0336d95d2a4568ab919877b579
joan-KIM <3373minji.kim@gmail.com>
2022-02-05 02:35:43 +0900

    타임라인 디자인 CSS 적용

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 import DatePicker from "./DatePicker";
+import {MdAdd} from "react-icons/md";
 
 export default function Calendar({date, changeHandler}){
     return (
         <div className='calendar-section' >
             <div className='calendar-container'>
                <DatePicker selectedDate={date} changeHandler={changeHandler}></DatePicker>
+
+               <button>Add Event<MdAdd /></button>
+               
+               <ul className='timeline'>
+                    <li className='timeline-item'>
+                        <div className='timeline-point'></div>
+                        <div className='event-wrapper'>
+                            <span className='event-title'>치과 정기검진</span>
+                            <span className='event-time'>13:30 - 14:30</span>
+                        </div>
+                        <p className='event-place'>아름다운 이치과의원</p>
+                    </li>
+                    <li className='timeline-item'>
+                        <div className='timeline-point'></div>
+                        <div className='event-wrapper'>
+                            <span className='event-title'>아르바이트</span>
+                            <span className='event-time'>17:00 - 22:00</span>
+                        </div>
+                        <p className='event-place'>하삼동커피 기장떙땡점</p>
+                    </li>
+               </ul>
             </div>
         </div>
     )
