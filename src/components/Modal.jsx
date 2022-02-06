6d3aa018d923c8197f4b52c76b20be4eb93ce740
joan-KIM <3373minji.kim@gmail.com>
2022-02-07 03:39:19 +0900

    시간 선택 박스 마크업, 디자인 적용

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { MdOutlineDelete } from "react-icons/md";
+import { MdOutlineDelete, MdUnfoldMore } from "react-icons/md";
 import classNames from "classnames";
 
 export default function Modal(){
@@ -21,8 +21,20 @@ export default function Modal(){
 
                     <div className="event-input-group">
                         <label for="event-time">Time</label>
-                        <div className="event-inputbox-wrapper">
-                            <input type="text" id="event-time"></input>
+                        <div className="timepicker-group">
+                            <div className="timepicker-wrapper">
+                                <input value="00"/>
+                                <MdUnfoldMore className="time-select-btn" />
+                                <input value="00"/>
+                                <MdUnfoldMore className="time-select-btn" />
+                            </div>
+                            <span>~</span>
+                            <div className="timepicker-wrapper">
+                                <input value="00" />
+                                <MdUnfoldMore className="time-select-btn" />
+                                <input value="00" />
+                                <MdUnfoldMore className="time-select-btn" />
+                            </div>
                         </div>
                     </div>
 
