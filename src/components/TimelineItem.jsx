93e0cce464e17dd174a5a71f956223750f5a0b0f
joan-KIM <3373minji.kim@gmail.com>
2022-02-13 03:31:54 +0900

    이벤트 장소 아이콘 추가, 타임라인 hover CSS 추가

--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { toTimeLabel } from "../utils/time";
+import {MdOutlinePlace} from "react-icons/md";
 
 export default function TimelineItem({event, onClick}) {
     return  <li className='timeline-item' onClick={onClick} >
@@ -12,6 +13,9 @@ export default function TimelineItem({event, onClick}) {
                     <span className='event-title'>{event.title}</span>
                     <span className='event-time'>{toTimeLabel(event.startTime)} - {toTimeLabel(event.endTime)}</span>
                 </div>
-                <p className='event-place'>{event.place}</p>
+                <div style={{ display:'flex'}}>
+                    <MdOutlinePlace className='place-icon' />
+                    <p className='event-place'>{event.place}</p>
+                </div>
             </li>
 }
