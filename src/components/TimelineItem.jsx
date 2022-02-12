85f9f90bf3ae7c7950a38136db321da1ce1b447f
joan-KIM <3373minji.kim@gmail.com>
2022-02-13 02:09:54 +0900

    타임라인 point CSS 수정

--- src/components/TimelineItem.jsx
+++ src/components/TimelineItem.jsx
@@ -3,8 +3,11 @@ import { toTimeLabel } from "../utils/time";
 
 export default function TimelineItem({event, onClick}) {
     return  <li className='timeline-item' onClick={onClick} >
-                <div className='timeline-point'
-                    style={{backgroundColor: event.category.color}} />
+                <div className='timeline-point-wrapper'
+                        style={{backgroundColor: `${event.category.color}1f`}}  >
+                    <div className='timeline-point'
+                        style={{backgroundColor: event.category.color}} />
+                </div>
                 <div className='event-wrapper'>
                     <span className='event-title'>{event.title}</span>
                     <span className='event-time'>{toTimeLabel(event.startTime)} - {toTimeLabel(event.endTime)}</span>
