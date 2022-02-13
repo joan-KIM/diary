9ec719632e114dff64a64995e74f7c879b1a0e4c
joan-KIM <3373minji.kim@gmail.com>
2022-02-14 01:28:57 +0900

    배포

--- src/App.js
+++ src/App.js
@@ -6,36 +6,7 @@ import {today} from "./utils/date"
 import { uuid } from "./utils/uuid";
 
 function App() {
-  const [data, setData] = useState({
-    '22/2/13' : {
-      tasks: [
-        {
-          id: uuid(),
-          title: '이벤트 등록',
-          state: false
-        }
-      ],
-      note: {
-        title: '우아아아?',
-        content: '돌아가라 머리머리'
-      },
-      events: [
-        {
-          id: uuid(),
-          title: '아르바이트',
-          place: '기장고촌 ㅇㅇㅇ커피',
-          startTime: 1020,
-          endTime: 1320,
-          category: {
-            'name':'Personal',
-            'color':'#7367f0'
-          }
-        }
-      ]
-    }
-  }
-);
-
+  const [data, setData] = useState({});
   const [date, setDate] = useState(today());
 
   const changeHandler = (value) => {
